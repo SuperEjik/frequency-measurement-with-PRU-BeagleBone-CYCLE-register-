d3978eff21b00195cdaa1237f46d93a9d7fba2f3
SuperEjik <63389802+SuperEjik@users.noreply.github.com>
2022-04-13 16:56:59 +0300

    update measure frequency

--- out_test.c
+++ out_test.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <sys/mman.h>
-
+#include <math.h>
+#include <unistd.h>
 #define MAXCH 4
 
 #define PRU_ADDR        0x4A300000      // Start of PRU memory Page 184 am335x TRM
@@ -13,22 +14,27 @@
 unsigned int    *pru0DRAM_32int_ptr;        // Points to the start of local DRAM
 unsigned int    *pru1DRAM_32int_ptr;        // Points to the start of local DRAM
 unsigned int    *prusharedMem_32int_ptr;    // Points to the start of the shared memory
+unsigned int    *T;
 
 int start_pwm_count(int ch, int countOn, int countOff) {
     unsigned int *pruDRAM_32int_ptr = pru0DRAM_32int_ptr;
 
     // write to PRU shared memory
     printf("\n");
-    printf("%d\n",pruDRAM_32int_ptr[2*(ch)+0]);  // On time
+    printf("%lf\n",1/(pruDRAM_32int_ptr[2*(ch)+0]*2.5*pow(10,-10)) );  // On time
     printf("\n");
     return 0;
 }
 
 int main(int argc, char *argv[])
 {
+    //while(1){
     unsigned int    *pru;       // Points to start of PRU memory.
     int fd;
-
+    int countF = 0;
+    double F = 0.0;
+    //while(countF < 1000)
+    //{
     fd = open ("/dev/mem", O_RDWR | O_SYNC);
  if (fd == -1) {
         printf ("ERROR: could not open /dev/mem.\n\n");
@@ -40,11 +46,31 @@ int main(int argc, char *argv[])
         return 1;
     }
     close(fd);
-
-    pru0DRAM_32int_ptr =     pru + PRU0_DRAM/4 + 0x200/4;   // Points to 0x200 of PRU0 memory
+ pru0DRAM_32int_ptr =     pru + PRU0_DRAM/4 + 0x200/4;   // Points to 0x200 of PRU0 memory
     pru1DRAM_32int_ptr =     pru + PRU1_DRAM/4 + 0x200/4;   // Points to 0x200 of PRU1 memory
     prusharedMem_32int_ptr = pru + PRU_SHAREDMEM/4; // Points to start of shared memory
 
     int i;
-    start_pwm_count(i, i+1, 20-(i+1));
+    //start_pwm_count(i, i+1, 20-(i+1));
+
+    unsigned int *pruDRAM_32int_ptr = pru0DRAM_32int_ptr;
+
+    F = 1/(pruDRAM_32int_ptr[0]*49.83240366*pow(10,-10));
+
+    //printf("\n");
+    printf("%lf\n",F);
+    //printf("%d\n",pruDRAM_32int_ptr[0]);
+    //printf("\n");
+
+    //countF++;
+    //}
+    //printf("\n");
+    //printf("%lf\n",F/1000 );
+    //printf("\n");
+
+    /*printf("\n");
+    printf("%lf\n",1/(pruDRAM_32int_ptr[0]*2.5*pow(10,-10)) );
+    printf("\n");*/
+    //sleep(1);
+    //}
 }
