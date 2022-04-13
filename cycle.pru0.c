3b560270dd7eac8b0a6a4e461f046fa82dadf4f7
SuperEjik <63389802+SuperEjik@users.noreply.github.com>
2022-04-13 16:53:56 +0300

    update cycle

--- cycle.pru0.c
+++ cycle.pru0.c
@@ -10,34 +10,48 @@
 
 #define PRU0_DRAM       0x00000         // Offset to DRAM
 volatile unsigned int *pru0_dram = (unsigned int *) (PRU0_DRAM + 0x200);
+//volatile double *pru0_dram = (double *) (PRU0_DRAM + 0x200);
 
-volatile register uint32_t __R30;
+//volatile register uint32_t __R30;
 volatile register uint32_t __R31;
 
-#define P9_31 (1 << 0)
+#define P8_15 (1 << 15)
 
 void main(void)
 {
     uint32_t cycle;
+    uint32_t zero;
+    int count=0;
 
-    CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
+    //CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
 
-    //PRU0_CTRL.CTRL_bit.CTR_EN = 1;
-    //PRU0_CTRL.CYCLE = cycle;
+    while(__R31 & P8_15)
+    {
+    }
 
+    while((__R31 & P8_15)==0)
+    {
+    }
+
+    PRU0_CTRL.CTRL_bit.CTR_EN = 1;
     while (1)
     {
-        PRU0_CTRL.CTRL_bit.CTR_EN = 1;
-        PRU0_CTRL.CYCLE = cycle;
-        while(__R31 & P8_15)
-        {
-        }
+        //if(__R31 & P8_15)
+        //{
 
-        while((__R31 & P8_15)==0)
-        {
-        }
-
-        cycle = PRU0_CTRL.CYCLE;
-        pru0_dram[2*0 ] = 1000000/(cycle/200);
+        //while(count<1000)
+        //{
+            //PRU0_CTRL.CTRL_bit.CTR_EN = 1;
+            PRU0_CTRL.CYCLE = zero;
+            while(__R31 & P8_15)
+            {
+            }
+            while((__R31 & P8_15)==0)
+            {
+            }
+            cycle = PRU0_CTRL.CYCLE;
+        //}
+        //count=0;
+        pru0_dram[2*0 ] = (cycle);
     }
 }
